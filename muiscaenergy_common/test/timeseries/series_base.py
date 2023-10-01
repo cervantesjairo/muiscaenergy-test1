cb07aa115eab055cf68fd4ef84fe6f11c6be076f
jc <jhcervantesg@gmail.com>
2023-10-01 11:02:32 -0400

    v1

--- muiscaenergy_common/test/timeseries/series_base.py
+++ muiscaenergy_common/test/timeseries/series_base.py
@@ -5,25 +5,25 @@ from muiscaenergy_common.src.timeseries.series_base import TimeSeries as TS
 
 class TimeSeriesTest(unittest.TestCase):
 
-    def test_get_by_freq(self):
+    def test_get_ts(self):
         ts = TS(ts_from=datetime(2022, 4, 10),
                 ts_to=datetime(2022, 4, 11),
                 freq='15T')
 
-        df = ts.get_by_freq()
+        df = ts.get_ts()
 
         self.assertFalse(df.empty)
         self.assertTrue('datetime_local_from' in df.columns)
         self.assertTrue('datetime_local_to' in df.columns)
 
-    def test_get_by_tz(self):
+    def test_get_ts_tz(self):
         # tz = "America/New_York"; tz = "America/Los_Angeles"; tz = "America/Chicago";
         ts = TS(ts_from=datetime(2022, 4, 10),
                 ts_to=datetime(2022, 4, 11),
                 freq='H',
                 tz="America/Los_Angeles")
 
-        df = ts.get_by_tz()
+        df = ts.get_ts_tz()
 
         self.assertFalse(df.empty)
         self.assertTrue('datetime_utc' in df.columns)
@@ -38,7 +38,7 @@ class TimeSeriesTest(unittest.TestCase):
                 freq='2D',
                 lat=lat,
                 lon=lon)
-        df = ts.get_by_latlon()
+        df = ts.get_ts_latlon()
 
         self.assertFalse(df.empty)
         self.assertTrue('datetime_utc' in df.columns)
